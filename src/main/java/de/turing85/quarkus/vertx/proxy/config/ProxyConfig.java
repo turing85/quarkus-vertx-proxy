b2707d54c07eaa65e2578fbef450b1f6abf33ad4
Marco Bungart <marco.bungart@googlemail.com>
2025-09-25 17:01:32 +0200

    Remove @ConfigRoot annotation

--- src/main/java/de/turing85/quarkus/vertx/proxy/config/ProxyConfig.java
+++ src/main/java/de/turing85/quarkus/vertx/proxy/config/ProxyConfig.java
@@ -1,12 +1,10 @@
 package de.turing85.quarkus.vertx.proxy.config;
 
-import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
 @ConfigMapping(prefix = "quarkus.vertx-proxy")
-@ConfigRoot
 public interface ProxyConfig {
   int DEFAULT_PORT = 8880;
   int DEFAULT_TEST_PORT = 8881;
